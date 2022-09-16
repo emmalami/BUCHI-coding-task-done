b5180db1d46b07176e0a0da94b63efc876a5c390
Olamide John <olamide.john@outlook.com>
2022-09-16 11:35:48 +0100

    set up github Pipeline

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ function converter(ft) {
 
 // 3. Print all odd numbers between 1 and 100
 function printOdd() {
- for (let i = 1; i <= 100; i++) {
+ for (let i = 1; i <= 10; i++) {
   if (i % 2 !== 0) {
    console.log(i);
   }
